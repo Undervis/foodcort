7beea49697a1f8ee1aec619b86e0931a2f258045
Undervis <undervis@undervis>
2022-11-29 22:23:06 +0500

    update docs

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.openapi.utils import get_openapi
 from models import *
 import db
 
-app = FastAPI()
+tags_meta = [
+    {
+        "name": "Dishes",
+        "description": "Функции для взаимодействия с блюдами"
+    },
+    {
+        "name": "Auth",
+        "description": "Функции для авторизации пользователя(вход и регистрация)"
+    },
+    {
+        "name": "Cart",
+        "description": "Функции для взаимодействия с корзиной"
+    }
+]
+
+app = FastAPI(
+    title="Food delivery API",
+    version="1.0",
+    contact={
+        "name": "Всесоздатель",
+        "url": "https://vk.com/theundervis",
+        "email": "k.lychkovsky.undervis@gmail.com"
+    }, openapi_tags=tags_meta
+)
 
 
-@app.get("/")
+@app.get("/", description="Для теста API")
 async def root():
     return {"message": "Ну чо могу сказать, апи работает"}
 
 
-@app.post("/add_dish", status_code=200)
+@app.post("/add_dish", description="Для добавления нового блюда", tags=['Dishes'])
 async def add_dish_request(dish: Dish):
     try:
         db.add_dish(name=dish.name, category=dish.category, price=dish.price, img=dish.img,
@@ -20,7 +44,7 @@ async def add_dish_request(dish: Dish):
         raise HTTPException(status_code=400, detail={"message": "Что-то пошло не так"})
 
 
-@app.get('/dishes')
+@app.get('/dishes', description="Вывод списка блюд", tags=['Dishes'])
 async def get_dishes_request():
     dishes = []
     for dish in db.get_dishes():
@@ -29,7 +53,7 @@ async def get_dishes_request():
     return dishes
 
 
-@app.get("/get_dish/{dish_id}")
+@app.get("/get_dish/{dish_id}", description="Получение блюда по его id", tags=['Dishes'])
 async def get_dish_request(dish_id: int):
     dish = db.get_dish(dish_id)
     if dish:
@@ -40,13 +64,13 @@ async def get_dish_request(dish_id: int):
         raise HTTPException(status_code=400, detail={"message": "Такого блюда не нашлось"})
 
 
-@app.delete("/delete_dish/{dish_id}")
+@app.delete("/delete_dish/{dish_id}", description="Удаление блюда по его id", tags=['Dishes'])
 async def delete_dish_request(dish_id: int):
     db.delete_dish(dish_id)
     return {"message": "Блюдо съели", "error": 0}
 
 
-@app.post("/auth/register")
+@app.post("/auth/register", description="Регистрация нового пользователя", tags=["Auth"])
 async def auth_register_request(user: UserRegister):
     if db.auth_register(user.email, user.login, user.password, user.phone):
         return {"message": "Пользователь успешно зарегистрирован", "token": db.auth_login(user.email, user.password)[0]}
@@ -54,7 +78,7 @@ async def auth_register_request(user: UserRegister):
         raise HTTPException(status_code=400, detail={"message": "Пользователь с таким логином или почтой уже есть"})
 
 
-@app.post("/auth/login")
+@app.post("/auth/login", description="Авторизация зарегистрированного пользователя", tags=["Auth"])
 async def auth_login_request(user: UserLogin):
     token = db.auth_login(user.email, user.password)
     if token:
@@ -63,7 +87,7 @@ async def auth_login_request(user: UserLogin):
         raise HTTPException(status_code=400, detail={"message": "Неверный логин или пароль"})
 
 
-@app.post("/add_to_cart")
+@app.post("/add_to_cart", description="Добавление блюда в корзину пользователя", tags=["Cart"])
 async def add_to_cart_request(user_token: int, dish_id: int):
     try:
         db.add_to_cart(user_token, dish_id)
@@ -72,7 +96,7 @@ async def add_to_cart_request(user_token: int, dish_id: int):
         raise HTTPException(status_code=400, detail={"message": "Что-то пошло не так"})
 
 
-@app.get("/get_cart/{token}")
+@app.get("/get_cart/{token}", description="Получение корзины пользователя", tags=["Cart"])
 async def get_cart_request(token: int):
     cart = []
     for item in db.get_cart(token):
@@ -80,7 +104,7 @@ async def get_cart_request(token: int):
     return cart
 
 
-@app.delete("/delete_from_cart")
+@app.delete("/delete_from_cart", description="Удаление блюда из корзины пользователя", tags=["Cart"])
 async def delete_from_cart_request(item_id: int, user_token: int):
     db.delete_from_cart(item_id, user_token)
     return {"message": "Блюдо удалено из корзины"}
